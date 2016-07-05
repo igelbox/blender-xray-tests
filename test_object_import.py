1ef14e2f88438f9019aa409fccdfee91a83ca818
Vakhurin Sergey <igelbox@gmail.com>
2016-07-05 03:37:10 +0300

    tests: import bones with & without shapes

--- test_object_import.py
+++ test_object_import.py
@@ -6,10 +6,26 @@ import re
 
 class TestObjectImport(utils.XRayTestCase):
     def test_import_broken(self):
-        with utils.OpReportCatcher() as reports:
-            bpy.ops.xray_import.object(
-                directory=self.relpath(),
-                files=[{'name': 'test_import_broken.object'}],
-            )
-            reports.assertContains('WARNING', re.compile('Unsupported bone (\w+) shape type'))
-            reports.assertContains('WARNING', re.compile('Unsupported bone (\w+) ikjoint type'))
+        bpy.ops.xray_import.object(
+            directory=self.relpath(),
+            files=[{'name': 'test_import_broken.object'}],
+        )
+        self.assertReportsContains('WARNING', re.compile('Unsupported bone (\w+) shape type'))
+        self.assertReportsContains('WARNING', re.compile('Unsupported bone (\w+) ikjoint type'))
+
+    def test_import_normal(self):
+        bpy.ops.xray_import.object(
+            directory=self.relpath(),
+            files=[{'name': 'test_import.object'}],
+        )
+        self.assertReportsNotContains('WARNING')
+        self.assertIsNotNone(bpy.context.active_object.pose.bones[0].custom_shape)
+
+    def test_import_no_bone_shapes(self):
+        bpy.ops.xray_import.object(
+            directory=self.relpath(),
+            files=[{'name': 'test_import.object'}],
+            shaped_bones=False
+        )
+        self.assertReportsNotContains('WARNING')
+        self.assertIsNone(bpy.context.active_object.pose.bones[0].custom_shape)
