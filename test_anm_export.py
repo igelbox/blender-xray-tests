7109359eff3a100af1b05d6bfe78c227cd0f65b0
Vakhurin Sergey <igelbox@gmail.com>
2017-03-03 01:32:58 +0300

    tests: updated expectations for behavior of export anm

--- test_anm_export.py
+++ test_anm_export.py
@@ -6,8 +6,17 @@ import bpy
 class TestAnmExport(utils.XRayTestCase):
     blend_file = 'test_object_export.blend'
 
-    def test_error(self):
+    def test_error_no_anim(self):
         bpy.context.scene.objects.active = bpy.context.scene.objects['Cube1']
+        with self.assertRaisesRegex(Exception, 'Animation: object doesn\'t any animation data'):
+            bpy.ops.xray_export.anm(
+                filepath=self.outpath('Cube1.anm'),
+            )
+
+    def test_error_yxz(self):
+        obj = bpy.context.scene.objects['Cube1']
+        obj.animation_data_create().action = bpy.data.actions[0]
+        bpy.context.scene.objects.active = obj
         with self.assertRaisesRegex(Exception, "Animation: rotation mode must be 'YXZ'"):
             bpy.ops.xray_export.anm(
                 filepath=self.outpath('Cube1.anm'),
